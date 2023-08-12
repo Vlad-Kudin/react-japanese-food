cfd7dd9438a870bc6bf2ad473bead4bd68a18ce0
Vlad-Kudin <wolf_od_ua@yahoo.com>
2023-08-12 13:33:23 +0300

    added 'Cart'

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,26 @@
+import styles from "./Cart.module.css";
+
+const Cart = (props) => {
+
+  const CartItems = (
+    <ul className={styles['cart-items']}>
+      {[{id: 'm1', name: 'Sushi', amount: 2, price: 10.99}].map(item => <li>{item.name}</li>)}
+    </ul>
+  );
+
+  return (
+    <div>
+      {CartItems}
+      <div className={styles.total}>
+        <span>Итого</span>
+        <span>49.99$</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles['button--alt']}>Закрыть</button>
+        <button className={styles.button}>Заказать</button>
+      </div>
+    </div>
+  )
+}
+
+export default Cart;
