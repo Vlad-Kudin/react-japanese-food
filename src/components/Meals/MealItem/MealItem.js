769b1edd7d8b8c02032c2da19575b6b91fc30378
Vlad-Kudin <wolf_od_ua@yahoo.com>
2023-08-12 13:17:22 +0300

    added form

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import styles from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
     const formatedPrice = `$${props.price.toFixed(2)}`;
@@ -10,7 +11,9 @@ const MealItem = (props) => {
                 <div className={styles.description}>{props.description}</div>
                 <div className={styles.price}>{formatedPrice}</div>
             </div>
-            <div></div>
+            <div>
+              <MealItemForm id={props.id} />
+            </div>
         </li>
     );
 };
