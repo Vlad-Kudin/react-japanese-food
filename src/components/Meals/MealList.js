8f53335954275731387207eba407f98b0a5dd10c
Vlad-Kudin <wolf_od_ua@yahoo.com>
2023-10-17 17:43:05 +0300

    added error processing

--- src/components/Meals/MealList.js
+++ src/components/Meals/MealList.js
@@ -6,11 +6,17 @@ import MealItem from "./MealItem/MealItem";
 const MealList = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [httpErrorMessage, setHttpErrorMessage] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       setIsLoading(true);
       const response = await fetch('https://react-course-http-a4f50-default-rtdb.firebaseio.com/meals.json');
+
+      if (!response.ok) {
+        throw new Error("Error! Nothing not loaded!");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -28,13 +34,24 @@ const MealList = () => {
       setIsLoading(false);
     }
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpErrorMessage(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={styles.loading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpErrorMessage) {
+    return (
+      <section className={styles.error}>
+        <p>{httpErrorMessage}</p>
       </section>
     );
   }
