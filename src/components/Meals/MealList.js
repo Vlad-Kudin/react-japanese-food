84b02451694413016099d72bc57163ff3d61fa89
Vlad-Kudin <wolf_od_ua@yahoo.com>
2023-10-17 17:08:11 +0300

    added loading state

--- src/components/Meals/MealList.js
+++ src/components/Meals/MealList.js
@@ -4,11 +4,12 @@ import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
 const MealList = () => {
-
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchMeals = async () => {
+      setIsLoading(true);
       const response = await fetch('https://react-course-http-a4f50-default-rtdb.firebaseio.com/meals.json');
       const responseData = await response.json();
 
@@ -24,10 +25,19 @@ const MealList = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     }
 
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={styles.loading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
   const mealList = meals.map((meal) => (
     <MealItem
